b156f1d825b9ac838121e1b809cd72462735e351
maimaimai-bit <hmaivu02@gmail.com>
2025-02-04 01:37:46 -0600

    commit

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,50 @@
+class GameController < ApplicationController
+  def rules
+    render({ :template => "game/rules.html.erb" })
+  end
+
+  def rock
+    @user_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+    render({ :template => "game/play.html.erb" })
+  end
+
+  def paper
+    @user_move = "paper"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+
+    render({ :template => "game/play.html.erb" })
+  end
+
+  def scissors
+    @user_move = "scissors"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "game/play.html.erb" })
+  end
+end 
